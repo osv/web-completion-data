a0afde0c081444d4834267cfcecbf8b65846f5a6
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2016-02-26 18:52:41 +0200

    create tag files

--- convertor/build.js
+++ convertor/build.js
@@ -3,6 +3,8 @@
 var argv = require('minimist')(process.argv.slice(2));
 var mergeYamls = require('./lib/merger.js'),
     colors = require('colors'),
+    mkdirp = require('mkdirp'),
+    path = require('path'),
     yaml = require('js-yaml'),
     fs = require('fs'),
     _ = require('underscore');
@@ -11,9 +13,20 @@ if (! argv._.length || !argv.out) {
   console.log('Usage: build.js --out <dir> file1.yaml ...');
 }
 
+var FILE_TAG_LIST = 'html-tag-list',
+    DIR_TAGS_DOC = 'html-tag-short-docs',
+    DIR_ATTRIBUTES_VALUES = 'html-attributes-complete',
+    DIR_ATTRIBUTES = 'html-attributes-list',
+    DIR_ATTRIBUTES_LARGE_DOC = 'html-attributes-short-docs';
+    
 function main(argv) {
   var yamls = _.map(argv._, loadYAML);
-  var mergedYamls = mergeYamls(yamls);
+  var data = mergeYamls(yamls);
+
+  createStuffFiles(data, argv.out);
+  
+  //console.log(data);
+
   console.log(colors.green('Done.'));
 
 }
@@ -25,6 +38,47 @@ function loadYAML(file) {
     filename: file,
     data: yaml.safeLoad(data)
   };
+}
+
+// create html stuff. They are sorted ABC
+function createStuffFiles(data, dir) {
+  var tags = data.tags,
+      attributes = data.attributes;
+
+  var tagFile = path.join(dir, FILE_TAG_LIST);
+  var tagDocDir = path.join(dir, DIR_TAGS_DOC);
+  
+  mkdirp.sync(dir);
+  mkdirp.sync(tagDocDir);
+  
+  if (! _.isEmpty(tags)) {
+    var tagsData = _
+          .keys(tags)
+          .sort()
+          .join('\n');
+    
+    fs.writeFileSync(tagFile, tagsData);
+    created(tagFile);
+
+    var sortedTagNames = _.keys(tags).sort();
+
+    sortedTagNames.forEach(function createTagsDoc(tagName) {
+      var file = path.join(tagDocDir, tagName);
+      var doc = tags[tagName];
+      
+      if (!_.isEmpty(doc)) {
+        fs.writeFileSync(file, doc);
+        created(file);
+      }
+    });
+
+    console.log('');
+
+  }
+}
+
+function created(msg) {
+  console.log(colors.green('Created') + ' ' + msg);  
 }
 
 main(argv);
